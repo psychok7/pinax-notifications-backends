e8c0cd2d31dfabd2d433e11ffeaee5b8d1c06414
Nuno Khan <nunok7@gmail.com>
2016-11-30 14:35:57 +0000

    changed from rst to md

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     author_email="nunok7@gmail.com",
     description="Django application that implements a few backends for pinax-notifications",
     name="pinax-notifications-backends",
-    long_description=read("README.rst"),
+    long_description=read("README.md"),
     version="0.1",
     url="http://pinax-notifications.rtfd.org/",
     license="MIT",
