f7e1a3c0e23b991030d1f9a30a0416856140f6bc
Nuno Khan <nunok7@gmail.com>
2016-12-05 14:47:03 +0000

    sms backend working and improved push notification backend as well

--- pinax/notifications_backends/backends/push_notifications.py
+++ pinax/notifications_backends/backends/push_notifications.py
@@ -5,19 +5,42 @@ from _ssl import SSLError
 from .base import BaseBackend
 
 
+GCMDevice = get_class_from_path(
+    path='push_notifications.models.GCMDevice')
+APNSDevice = get_class_from_path(
+    path='push_notifications.models.APNSDevice')
+
+GCMError = get_class_from_path(
+    path='push_notifications.gcm.GCMError')
+APNSError = get_class_from_path(
+    path='push_notifications.apns.APNSError')
+
+try:
+    use_notice_model = getattr(settings, 'PINAX_USE_NOTICE_MODEL')
+except AttributeError:
+    use_notice_model = None
+
+
 class PushNotificationBackend(BaseBackend):
     spam_sensitivity = 2
 
     def can_send(self, user, notice_type, scoping):
-        can_send = super(PushNotificationBackend, self).can_send(user, notice_type, scoping)
-        if can_send and user.email:
+        can_send = super(
+            PushNotificationBackend, self).can_send(user, notice_type, scoping)
+
+        gcm_device_exists = GCMDevice.objects.filter(user=user).exists()
+        apns_device_exists = APNSDevice.objects.filter(user=user).exists()
+
+        if can_send and (gcm_device_exists or apns_device_exists):
             return True
         return False
 
     def deliver(self, recipient, sender, notice_type, extra_context):
         context = self.default_context()
         context['current_site'] = context['current_site'].domain
-        extra_context['aps'] = str(extra_context['aps']).encode("utf-8")
+        if extra_context.get('aps'):
+            extra_context['aps'] = str(extra_context['aps']).encode("utf-8")
+
         context.update(extra_context)
 
         if context.get('subject') and context.get('body'):
@@ -25,38 +48,30 @@ class PushNotificationBackend(BaseBackend):
                 context['subject'] + '\n\n' + context['body']
             )
 
-        GCMDevice = get_class_from_path(
-            path='push_notifications.models.GCMDevice')
-        APNSDevice = get_class_from_path(
-            path='push_notifications.models.APNSDevice')
+            gcm_device = GCMDevice.objects.filter(user=recipient)
+            apns_device = APNSDevice.objects.filter(user=recipient)
 
-        GCMError = get_class_from_path(path='push_notifications.gcm.GCMError')
-        APNSError = get_class_from_path(path='push_notifications.apns.APNSError')
+            if gcm_device:
+                try:
+                    gcm_device.first().send_message(None, extra=context)
+                except GCMError as e:
+                    print('GCMError "%s"', str(e))
 
-        gcm_device = GCMDevice.objects.filter(user=recipient)
-        apns_device = APNSDevice.objects.filter(user=recipient)
+            if apns_device:
+                try:
+                    apns_device.first().send_message(None, extra=context)
+                except (APNSError, SSLError) as e:
+                    print('APNSError "%s"', str(e))
 
-        if gcm_device:
-            try:
-                gcm_device.first().send_message(None, extra=context)
-            except GCMError as e:
-                print('GCMError "%s"', str(e))
+            if use_notice_model:
+                Notice = get_class_from_path(
+                    path='pinax.notifications.models.Notice')
 
-        if apns_device:
-            try:
-                apns_device.first().send_message(None, extra=context)
-            except (APNSError, SSLError) as e:
-                print('APNSError "%s"', str(e))
-
-        if settings.PINAX_USE_NOTICE_MODEL:
-            Notice = get_class_from_path(
-                path='pinax.notifications.models.Notice')
-
-            # Based on http://stackoverflow.com/a/7390947
-            # This is mostly a log for sent notifications.
-            if context.get('message'):
-                Notice.objects.create(
-                    recipient=recipient, message=context['message'],
-                    notice_type=notice_type, sender=sender,
-                    medium='push_notifications'
-                )
+                # Based on http://stackoverflow.com/a/7390947
+                # This is mostly a log for sent notifications.
+                if context.get('message'):
+                    Notice.objects.create(
+                        recipient=recipient, message=context['message'],
+                        notice_type=notice_type, sender=sender,
+                        medium='push_notifications'
+                    )
--- pinax/notifications_backends/backends/sms.py
+++ pinax/notifications_backends/backends/sms.py
@@ -1,4 +1,18 @@
+from django.conf import settings
+from pinax.notifications.utils import get_class_from_path
 from .base import BaseBackend
+from sendsms import api
+
+
+try:
+    use_notice_model = getattr(settings, 'PINAX_USE_NOTICE_MODEL')
+except AttributeError:
+    use_notice_model = None
+
+try:
+    mobile_phone_path = getattr(settings, 'PINAX_MOBILE_PHONE_PATH')
+except AttributeError:
+    mobile_phone_path = None
 
 
 class SmsBackend(BaseBackend):
@@ -6,10 +20,42 @@ class SmsBackend(BaseBackend):
 
     def can_send(self, user, notice_type, scoping):
         can_send = super(SmsBackend, self).can_send(user, notice_type, scoping)
-        if can_send and user.email:
+
+        if mobile_phone_path is not None:
+            mobile_phone = getattr(user, mobile_phone_path)
+        else:
+            userprofile = getattr(user, 'userprofile')
+            mobile_phone = getattr(userprofile, 'mobile_phone')
+
+        if can_send and mobile_phone:
             return True
         return False
 
     def deliver(self, recipient, sender, notice_type, extra_context):
-        print("SMS")
+        context = self.default_context()
+        context['current_site'] = context['current_site'].domain
+        context.update(extra_context)
 
+        if mobile_phone_path:
+            mobile_phone = getattr(recipient, mobile_phone_path)
+        else:
+            userprofile = getattr(recipient, 'userprofile')
+            mobile_phone = getattr(userprofile, 'mobile_phone').split(',')
+
+        if context.get('body'):
+            # from_phone is not important
+            api.send_sms(
+                body=context['body'], from_phone='+41791111111', to=mobile_phone
+            )
+
+            if use_notice_model:
+                Notice = get_class_from_path(
+                    path='pinax.notifications.models.Notice')
+
+                # Based on http://stackoverflow.com/a/7390947
+                # This is mostly a log for sent notifications.
+                Notice.objects.create(
+                    recipient=recipient, message=context['body'],
+                    notice_type=notice_type, sender=sender,
+                    medium='sms'
+                )
